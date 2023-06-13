1a7945406850214471ce72621f6b3a35ab5a401d
tvv1n <tvv1n@yahoo.com>
2023-06-13 12:34:45 +0300

    init project

---
+++ app/src/main/java/ua/com/tvv1n/weather/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.com.tvv1n.weather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
